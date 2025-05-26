4188717366ae0b89c7d0263c71e8b1d0d3f9acb1
Dmaravalalu <draveendra781@gmail.com>
2025-05-26 20:02:42 +0530

    code to check if we are in venv

---
+++ systest.py
@@ -0,0 +1,7 @@
+import sys
+print(sys.executable)
+
+#to check if we are in virtual env or global env. 
+# on running this : C:\Users\drave\Documents\P2P-blockchain\venv\Scripts\python.exe
+# notice how path has venv. hence its running in virutal env
+#all libraries downlaoded are downloded into this virtual env folder. open libs to check.
